6a492496e7938813925f176d242627e51f69e8b4
Diego Rojo <diegorojo@outlook.com>
2019-12-12 16:09:03 +0100

    Updates

---
+++ precache-manifest.9a36a5ca0314b2b07ac73aa02a771582.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "115c9efdea5db373348c3a5486872694",
+    "url": "/d5.3-ahmose/static/media/models_info.115c9efd.csv"
+  },
+  {
+    "revision": "c93eb794a947e0bfa0be93261137730e",
+    "url": "/d5.3-ahmose/static/media/models_data.c93eb794.csv"
+  },
+  {
+    "revision": "519f27d15b2b65e2fcf39ad64ea839a1",
+    "url": "/d5.3-ahmose/static/media/knowledge_intervals.519f27d1.csv"
+  },
+  {
+    "revision": "dae266277c11ea8e62807d895d423cc3",
+    "url": "/d5.3-ahmose/static/media/features_info.dae26627.csv"
+  },
+  {
+    "revision": "ef93d8eea0dce7843080",
+    "url": "/d5.3-ahmose/static/js/runtime~main.4b98848c.js"
+  },
+  {
+    "revision": "497d92259ad41cb6baa5",
+    "url": "/d5.3-ahmose/static/js/main.4796c0f3.chunk.js"
+  },
+  {
+    "revision": "da031a0ace6e731bd622",
+    "url": "/d5.3-ahmose/static/js/2.2e179922.chunk.js"
+  },
+  {
+    "revision": "497d92259ad41cb6baa5",
+    "url": "/d5.3-ahmose/static/css/main.44054207.chunk.css"
+  },
+  {
+    "revision": "da031a0ace6e731bd622",
+    "url": "/d5.3-ahmose/static/css/2.35467481.chunk.css"
+  },
+  {
+    "revision": "3d4badd0a766b8aae12acf7f3682c3f5",
+    "url": "/d5.3-ahmose/index.html"
+  }
+];
